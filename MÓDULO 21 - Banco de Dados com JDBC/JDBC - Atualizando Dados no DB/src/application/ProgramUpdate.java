42f093eca87a4b3406372dd4d84afe3906258108
DaniloADamasceno <danilo.engauto@gmail.com>
2023-02-06 14:33:12 -0300

    🚧 atualização de BD

---
+++ MÓDULO 21 - Banco de Dados com JDBC/JDBC - Atualizando Dados no DB/src/application/ProgramUpdate.java
@@ -0,0 +1,41 @@
+package application;
+
+import db.DBUpdate;
+import db.DbExceptionUpdate;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class ProgramUpdate {
+    public static void main(String[] args) {
+        System.out.println("Testing connection with database... 🆙 JDBC - Atualizando 🆙 Dados no DB ");
+        System.out.println();
+
+        Connection connUpdate = null;
+        PreparedStatement preStatment = null;
+
+        try{
+            connUpdate = DBUpdate.getConnection();
+            preStatment = connUpdate.prepareStatement(
+                    "UPDATE Seller "
+                            + "SET BaseSalary = BaseSalary + ?"
+                            + "WHERE "
+                            + "(DepartmentId = ?)");
+
+            preStatment.setDouble(1, 389);
+            preStatment.setInt(2, 4);
+
+            int AffectedRows = preStatment.executeUpdate();                                  // retorna o número de linhas afetadas
+
+            System.out.println(" Rows affected: /Linhas Afetadas pelo Update " + AffectedRows);
+
+        } catch (SQLException errUpdate) {
+            throw new DbExceptionUpdate(errUpdate.getMessage());
+        } finally {                                                                         // fechando a conexão com o banco de dados
+            DBUpdate.closeStatment(preStatment);
+            DBUpdate.closeConnection();
+        }
+
+    }
+}
