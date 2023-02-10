ce6beedf7633e431d1a0c3a2b43bf663ee9f019d
DaniloADamasceno <danilo.engauto@gmail.com>
2023-02-10 11:34:41 -0300

    🚧 Projeto com DAO SellerDAO

--- MÓDULO 21 - Banco de Dados com JDBC/Projeto com DAO/src/model/dao/DaoFactory.java
+++ MÓDULO 21 - Banco de Dados com JDBC/Projeto com DAO/src/model/dao/DaoFactory.java
@@ -2,9 +2,12 @@ package model.dao;
 
 import model.dao.implement.SellerDaoJDBC;
 
+import java.sql.Connection;
+
 public class DaoFactory {
 
     public static SellerDao createSellerDao() {
-        return new SellerDaoJDBC();
+        Connection connDao;
+        return new SellerDaoJDBC(connDao);
     }
 }
--- MÓDULO 21 - Banco de Dados com JDBC/Projeto com DAO/src/model/dao/implement/SellerDaoJDBC.java
+++ MÓDULO 21 - Banco de Dados com JDBC/Projeto com DAO/src/model/dao/implement/SellerDaoJDBC.java
@@ -3,9 +3,21 @@ package model.dao.implement;
 import model.dao.SellerDao;
 import model.entities.Seller;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.List;
 
 public class SellerDaoJDBC implements SellerDao {
+
+    private Connection connDao;
+
+    //-----------------------------------------------   Constructor   --------------------------------------------------
+    public SellerDaoJDBC(Connection connDao) {
+        this.connDao = connDao;
+    }
+
+    //-----------------------------------------------   Methods   ------------------------------------------------------
     @Override
     public void insert(Seller testSeller) {
 
@@ -23,6 +35,13 @@ public class SellerDaoJDBC implements SellerDao {
 
     @Override
     public Seller findById(Integer id) {
+
+        PreparedStatement prepStatment = null;
+        ResultSet resultSet = null;
+
+
+
+
         return null;
     }
 
