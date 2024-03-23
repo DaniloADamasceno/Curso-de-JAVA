677b8772e641649420f32e9b98b6e26c46a87a57
DANILO ALMEIDA DAMASCENO <danilo.engauto@gmail.com>
2024-03-23 19:58:07 -0300

    :recycle: Refatoração Módulo 18 -> 3

--- Módulo 18 - INTERFACES/Default Methods/src/services/BrazilInterestService.java
+++ Módulo 18 - INTERFACES/Default Methods/src/services/BrazilInterestService.java
@@ -1,34 +1,24 @@
 package services;
 
-public class BrazilInterestService implements InterestService_INTER {
+public class BrazilInterestService implements InterestServiceINTER {
 
-  private Double interestRate;
+    private Double interestRate;
 
-  //----------------------
-  //!       CONSTRUCTOR
-  //----------------------
-  public BrazilInterestService(Double interestRate) {
-    this.interestRate = interestRate;
-  }
+    //!   ----------------------------------    CONSTRUCTOR     --------------------------------------------------------
+    public BrazilInterestService(Double interestRate) {
+        this.interestRate = interestRate;
+    }
 
-  public BrazilInterestService() {
-  }
+    public BrazilInterestService() {
+    }
 
-  //----------------------------
-  //!       GETTERS AND SETTERS
-  //----------------------------
-  @Override
-  public double getInterestRate() {
-    return interestRate;
-  }
+    //!  ----------------------------------------    GETTERS AND SETTERS    --------------------------------------------
+    @Override
+    public double getInterestRate() {
+        return interestRate;
+    }
 
-  public void setInterestRate(Double interestRate) {
-    this.interestRate = interestRate;
-  }
-
-  //----------------------
-  //!       METHODS
-  //----------------------
-  
-  
+    public void setInterestRate(Double interestRate) {
+        this.interestRate = interestRate;
+    }
 }
--- Módulo 18 - INTERFACES/Exercicio de Fixação/src/service/ContractService.java
+++ Módulo 18 - INTERFACES/Exercicio de Fixação/src/service/ContractService.java
@@ -8,42 +8,35 @@ import java.util.Date;
 
 //import application.Payment_PayPal;
 
+public class ContractService {
 
+    private final ServiceOnlinePayment onlinePaymentService;
+    private Date date;
 
+    //!   ----------------------------------    CONSTRUCTOR     --------------------------------------------------------
+    public ContractService(ServiceOnlinePayment online_Payment_Service) {
+        this.onlinePaymentService = online_Payment_Service;
+    }
 
-      public class ContractService {
+    public void processContract(Contract contract, int months) {
+        double basicAlicota = contract.getTotalValue() / months;
+        for (int i = 1; i <= months; i++) {
+            double updateQuota = basicAlicota + onlinePaymentService.juros(basicAlicota, i);
+            double Cota_Maxima = updateQuota + onlinePaymentService.taxaJurosService(updateQuota);
+            contract.addParcelas(new Parcelas(date, Cota_Maxima));
+        }
+    }
 
-            private Online_Payment_Service online_Payment_Service;
-            private Date date;
+    //!   -------------------------------------------      METHODS      ------------------------------------------------
+    //%%    Adicionar meses a uma data
+    private Date addMonths(Date date, int n) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        cal.add(Calendar.MONTH, n);
+        return cal.getTime();
+    }
 
-            //----------------------------
-            //!       CONSTRUCTOR
-            //----------------------------
-
-            public ContractService(Online_Payment_Service online_Payment_Service) {
-                  this.online_Payment_Service = online_Payment_Service;
-            }
-
-            
-
-            public void processContract(Contract contract, int months) {
-                  double basicAlicota = contract.getTotalValue() / months;
-                  for (int i = 1; i <= months; i++) {
-                        double updateQuota = basicAlicota + online_Payment_Service.juros(basicAlicota, i);
-                        double Cota_Maxima = updateQuota + online_Payment_Service.taxa_Juros(updateQuota);
-                        contract.addParcelas(new Parcelas(date, Cota_Maxima));
-                  }
-            }
-
-            //%%---------->>> Método para adicionar meses a uma data
-            private Date addMonths(Date date, int n) {
-                  Calendar cal = Calendar.getInstance();
-                  cal.setTime(date);
-                  cal.add(Calendar.MONTH, n);
-                  return cal.getTime();
-            }
-
-      }
+}
 
    
 
